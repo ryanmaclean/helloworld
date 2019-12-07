5a1c44c52398c3768e875c2471674c75c6b00f91
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 19:38:32 -0800

    Create hello.rb

---
+++ hello.rb
@@ -0,0 +1,10 @@
+#!/usr/local/bin/ruby -w
+
+# Ruby was possibly my favourite way to write shells scripts, and somehow I've mostly forgotten the syntax. 
+# I was working as both an Apple bench tech at the time, as well as an ASCA-certified consultant. 
+# These scripts would kick off Xraid tasks, domain joins and run other scripts. I may have also 
+# dabbled with Rails, but honestly wasn't great at it. This is also around the same time I started
+# using emoticons in emails, having only ever used them in IRC, but realizing the benefit they added.
+# Microsoft Exchange used to drive me crazy with the 'J' it would substitute for ':)'.
+
+puts 'Hello world :)'
