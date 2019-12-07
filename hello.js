d2efdf10cd3b71ce58ecfb11d0288f9a0da4d457
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 19:41:29 -0800

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,5 @@
+// I played around with Javascript while learning Rails, having attended RailsConf in 2006, and taken a photo of DDH's
+// then-famous FU slide and posted it on Flickr. It didn't last long, and I don't think I realized that I had 
+// used a few things that were similar by this point. 
+
+document.write("Hello cruel world")
