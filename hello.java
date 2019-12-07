ada474419fea94afefd72314505e6c5cb85e5bf2
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 18:17:23 -0800

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,6 @@
+// This would have been Java 1.4, which had just come out
+// Fun fact: we had to memorize public static void main etc as our exams were on paper
+
+public static void main(String[] args){
+  System.out.println("Hello world");
+}
