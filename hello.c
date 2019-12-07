96997d6524daa903be029448b55692762ed4a031
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 20:10:04 -0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+// Similar enough to the C++ I learned in school to wrap my head around, 
+// though things like char* still throw me for a loop!
+// I typically only touch C for Arduino and mechanical keyboard stuff, editing in Clion.
+
+#include <stdio.h>
+
+int main() {
+   printf("Hello, world");
+   return 0;
+}
