56ace133d63706a0c5a56bd9f5c79719616d9912
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 19:30:30 -0800

    Create hello.pl

---
+++ hello.pl
@@ -0,0 +1,7 @@
+#! /usr/bin/perl
+
+# I'm pretty sure this is when I finally learned about the hashbang, and chmod +x. I used Perl for reports
+# directly on our remote colo server, I think to gather hit stats? I ended up using it a lot over the years
+# to do very hacky things - it had a wealth of scripts online that I could tease apart and mash together. 
+
+print "Hello world!\n";
