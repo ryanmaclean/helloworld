49a7b90f04f562442e067f858274868207a8160a
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 19:56:20 -0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# Though I had played with python scripts when setting up splogs, the first time I really had to write it was to help 
+# with Jenkins jobs. These were inline, and probably not the right approach, even at the time, but I got pretty 
+# far with it. 
+# This would also mark the first time I started using 'env' in my shebangs, somehow. 
+
+print("Hello world")
