bf15600aacb1c814fbe3008247e2bb3e5cf0a156
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 19:17:42 -0800

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,7 @@
+# This would have been PHP 4, and I desperately wished we could afford the Zend engine at our host, Magma. 
+# I spent WAY too much time in the PHP world, first with OSCommerce, then WordPress. I never did learn PHP5. 
+# Also, Gedit would have been my editor at this time, again in SuSE 8. On our server, I used Nano, fearing Vi!
+# (Emacs was not available) At some point I signed up for Dreamhost, where I loved using FTP and local text editors
+# to live-update my sites. 
+
+<?php echo "Hello World"; ?>
