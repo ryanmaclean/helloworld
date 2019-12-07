002552d0ec0af3ba5a61b786193fb752836f0e84
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 19:45:49 -0800

    Create hello.ps1

---
+++ hello.ps1
@@ -0,0 +1,7 @@
+<# I used and abused powershell scripts found around the net until I got official training in the form
+of EA credits from Microsoft redeamed at Global Knowledge for PowerShell 1, 2 and 4 courses. 
+I would typically send these to remote computers over psexec, and author everything in PowerShell ISE.
+These days I end up using it whenever I need to bootstrap something on Windows. 
+#>
+
+Write-Host "hello world"
