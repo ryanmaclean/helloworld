8df90dca080620fa48f41f2823baaedf81fa9337
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 18:42:54 -0800

    Create hello.sql

---
+++ hello.sql
@@ -0,0 +1,9 @@
+-- I fundamentally didn't understand why anyone would need to retrieve data at this time
+-- I had high speed internet at Dalhousie, and used it to Google ways I could make SQL
+-- do what I wanted: output to the console. Note that we didn't even have a DB to connect to,
+-- which explains my confusion a bit. Further, this three month class was C++, CCNA and SQL.
+
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('HELLO WORLD'); 
+END;
+\
