96daba2a1954d73a2a4f610477485683febf0e9f
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 19:58:48 -0800

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,15 @@
+// Still not a fan of the curly braces starting on a new line, but what can you do?!
+// Most of this was reading C# code in .net core that would run in Docker on Kubernetes. 
+
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
