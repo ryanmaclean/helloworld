14f39d66e619cd2d1e22f70df839f279ca925854
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 18:23:54 -0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// I absolutely HATED the curly braces on new lines in C++
+// Honestly, I'm still getting over it. Because I had Java pounded into my brain at 7:30AM every weekday for a year, 
+// I thought that it was the _one true syntax_ for quite some time. 
+
+#inclue <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello world";
+  return 0;
+}
