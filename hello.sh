ca96fa579625afc0f429c7a8dd84f279c389b0b2
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 19:27:09 -0800

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,5 @@
+# I still don't think I knew the difference between sh and bash at this point, though I knew I hated Korn shell
+# and wished I could have Bash on the unix workstations in HP-UX or Solaris. Once I found the echo syntax, 
+# I used it forever instead of printf. This would have been Bash 2. 
+
+echo hello world
