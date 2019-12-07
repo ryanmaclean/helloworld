d7d3da674791424cc0b091dfb8a83778108cad2b
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 18:20:02 -0800

    Create hello.el

---
+++ hello.el
@@ -0,0 +1,4 @@
+;; What I remember liking in Emacs was being able to make my own silly things that I thought sped up coding
+;; I'm still convinced I spent more time setting up functions in Emacs than I did learning Java...
+
+(insert "Hi world!")
