2c94f811265557fefb045572e490f587ae99fa4c
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 18:50:12 -0800

    Create hello.tcl

---
+++ hello.tcl
@@ -0,0 +1,14 @@
+# The great thing about the CCNA class is that we touched on TCL, and that I was a raving IRC addict by the year 2000
+# having used it while in junior high, high school and most importantly while in Japan. I was ^^Oo_oO^^ on undernet. 
+# I won't claim to have invented the whole ASCII face as username trend, but I definitely hadn't seen it before and
+# thought I was pretty clever!
+
+# TCL on Cisco devices didn't interest me at all, I didn't even use a router at home, just internet straight in
+# to a Mandrake box! Similar to the SQL problem, I didn't have a router or router sim in university, this was all theory
+# EggDrops let me write code and test them in IRC, exposing my public IP to the world. What a time to live!
+# Around this time I had discovered textfiles.com and thought mispellings were funny after readin das blinkenlights.
+# Source - http://textfiles.com/100/
+
+proc hello {} {
+puts "Hullo werld"
+}
