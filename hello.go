bbb18877bd698ab97580d4030148bffabc0c9720
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2019-12-06 20:04:39 -0800

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,11 @@
+// Finally, a syntax I like! Go feels like a mishmash of C, Java and Bash syntax to me. It's not for everyone,
+// but I have been enjoying it. Not using this for anything other than dirty API calls for popular SaaS services, writing
+// boilerplate tests for Terraform modules, but even that might be short-lived. Handy to be able to put in a Lambda/Function.
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!!!")
+}
